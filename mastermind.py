1e71b7cd59c92715fb0c2c41aa8d54202db017dd
Robert Jeffrey <robert.jeffrey@anu.edu.au>
2021-11-29 15:25:55 +1100

    Added commenting to mastermind

--- mastermind.py
+++ mastermind.py
@@ -3,17 +3,24 @@ import itertools
 from tqdm import tqdm
 from collections import defaultdict
 digits = range(10)
+
+# Guesses are represented as tuples of integers.
 all_guesses = list(itertools.combinations_with_replacement(digits, 6))
 possibilities = set(itertools.combinations_with_replacement(digits, 6))
 
 # %%
+# Given a guess and a proposed code, calculate the correct and close digits according to Mastermind rules.
 def score_guess(guess, code):
     i = 0
     j = 0
+    # Correct digits = number of identical digits in the same spots.
     correct_count = sum(1 if guess[i] == code[i] else 0 for i in range(len(guess)))
     
+    # Total shared digits = number of digits present in both strings
+    # = correct + close
     total_count = 0
 
+    # Code assumes both lists are ordered.
     while True:
 
         if i == len(guess):
@@ -39,6 +46,7 @@ assert score_guess((4,4,4,7,8,9), (4,6,6,8,9,9)) == (2,1)
 
 
 # %%
+# Update the possible codes given a guess & score.
 def update_consistent(guess, correct, close):
     global possibilities
     new_possibilities = set()
@@ -49,16 +57,23 @@ def update_consistent(guess, correct, close):
 
 # %%
 
+# Minimax solve the problem.
 def solve():
 
     best_guess = []
     best_score = 0
     minmax_remaining = len(possibilities)
+    # Consider each possible guess.
     for guess in tqdm(all_guesses):
+
+        # Consider each possible score the guess could get. How many possible codes would satisfy that?
         score_map = defaultdict(lambda: 0)
         for code in possibilities:
             score_map[score_guess(guess, code)] += 1
         
+        # The value of this guess is the minimum guarenteed number of codes that it would eliminate.
+        # Choose the guess that maximises this. I.e., the guess that has the largest guarenteed codes eliminated.
+        # Equivilent to the maximum remaining score, which we'd want to minimise.
         max_remaining = max(score_map.values())
         if max_remaining < minmax_remaining:
             best_guess = [guess]
@@ -73,6 +88,7 @@ def solve():
 
 # %%
 
+# Some helper code
 def check_guess(guess):
     score_map = defaultdict(lambda: 0)
     for code in possibilities:
